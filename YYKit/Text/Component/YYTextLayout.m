1801d80e8561e54b34f0ca40331f1528455527a2
Yown <990192315@qq.com>
2026-03-26 15:22:44 +0800

    Support Xcode 26.4

--- YYKit/Text/Component/YYTextLayout.m
+++ YYKit/Text/Component/YYTextLayout.m
@@ -1503,19 +1503,15 @@ fail:
     }
     
     [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
-        if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
-        } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
-        }
+        CGFloat distLeft = fabs(left - (isVertical ? point.y : point.x));
+        CGFloat distRight = fabs(right - (isVertical ? point.y : point.x));
+        position = (distLeft < distRight) ? prev : next;
     }];
     
     [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
-        if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
-        } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
-        }
+        CGFloat distLeft = fabs(left - (isVertical ? point.y : point.x));
+        CGFloat distRight = fabs(right - (isVertical ? point.y : point.x));
+        position = (distLeft < distRight) ? prev : next;
     }];
     
     if (position < _visibleRange.location) position = _visibleRange.location;
